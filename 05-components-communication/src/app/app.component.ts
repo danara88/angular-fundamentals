615a99851c12f92fc3d5f30341ebf64e931fa69c
Daniel Aranda <c-daniel.aranda@charter.com>
2024-09-09 13:05:34 -0600

    Add course sessions

---
+++ 05-components-communication/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = '05-components-communication';
+}
